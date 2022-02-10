fbc2d9306bdba8fa0e8ae461e71d318934f2e485
root <root@DESKTOP-5E92UIF.localdomain>
2022-02-10 13:46:16 +0100

    add footer

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Footer from '../components/Footer.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/Footer',
+    
+    component: Footer
   },
   {
     path: '/about',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,5 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Welcome to Your Vue.js App</h1>
+    <Footer />
   </div>
 </template>
+
+
+<script>
+import Footer from "../components/Footer"
+export default{
+components: {Footer}
+}
+
+</script>
