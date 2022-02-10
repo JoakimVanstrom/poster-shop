131ccec1af02ecfd30622f657cdd8dee036c4698
MohonaJS <mohona.chowdhury@iths.se>
2022-02-10 14:14:22 +0100

    add nav

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-   <Header></Header>
+   <Nav></Nav>
    
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Nav from './components/Nav.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Nav
   }
 }
 </script>
